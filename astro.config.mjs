73540f9f9c4d651db3c2047da1417d057c033ff4
Anas-HADDANE-NWS <ahaddane@normandiewebschool.fr>
2025-06-13 02:09:50 +0200

    deploiement github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://Anas1234566U.github.io/portfolioAstro',
   integrations: [mdx(), sitemap()],
 
   vite: {
